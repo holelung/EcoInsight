e98bfd1bff2917b314b72d0656e3bd2e76b66994
holelung <junho335276@gmail.com>
2025-04-14 14:24:40 +0900

    spring 초기세팅

---
+++ EcoInsight/src/main/java/com/ecoinsight/configuration/SecurityConfigure.java
@@ -0,0 +1,29 @@
+package com.ecoinsight.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfigure {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+		
+		return httpSecurity.formLogin(AbstractHttpConfigurer::disable)
+				.httpBasic(AbstractHttpConfigurer::disable)
+				.csrf(AbstractHttpConfigurer::disable)
+				.cors(Customizer.withDefaults())
+				.authorizeHttpRequests(requests -> {
+					requests.requestMatchers(HttpMethod.POST).permitAll();
+				})
+				.build();
+	}
+	
+}
