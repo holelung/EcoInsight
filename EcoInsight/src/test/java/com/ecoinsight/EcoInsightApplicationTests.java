4f4328b92aab9ec8d408900437ea5c603d97cc0c
holelung <junho335276@gmail.com>
2025-04-14 10:40:20 +0900

    project build

---
+++ EcoInsight/src/test/java/com/ecoinsight/EcoInsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoinsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoInsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
