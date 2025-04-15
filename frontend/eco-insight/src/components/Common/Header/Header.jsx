895d51efe4ed6d8105ff0909cc247efb5616a7a0
holelung <junho335276@gmail.com>
2025-04-15 12:50:14 +0900

    헤더 약간완성

--- frontend/eco-insight/src/components/Common/Header/Header.jsx
+++ frontend/eco-insight/src/components/Common/Header/Header.jsx
@@ -1,19 +1,32 @@
-
+import logo from '../../../assets/EcoInsigthLogo2.png'
 
 const Header = () => {
 
   return (
     <>
       <header className="w-full">
-        <div className="flex items-center justify-between px-6 py-3 bg-white shadow">
+        <div className="flex items-center justify-around px-6 py-5 mb-3 bg-white shadow">
           <div className="logo-area flex items-center space-x-2">
-            <img src="../../assets/EcoInsightLogo2.png" alt="EcoInsightLogo" className="h-8 w-auto"/>
+            <img
+              src={logo}
+              alt="EcoInsightLogo"
+              className="h-14 w-auto"
+            />
           </div>
           <div className="nav-area">
-
+            <ul className="flex space-x-4 py-3">
+              <li>소개합니다</li>
+              <li>대시보드</li>
+              <li>게시판</li>
+              <li>공지사항</li>
+              <li>문의하기</li>
+            </ul>
           </div>
           <div className="button-area flex items-center space-x-4">
-            <button></button>
+            <button className="px-4 py-2 rounded-lg bg-main">로그인</button>
+            <button className="px-4 py-2 rounded-lg bg-sub text-white">
+              회원가입
+            </button>
           </div>
         </div>
       </header>
