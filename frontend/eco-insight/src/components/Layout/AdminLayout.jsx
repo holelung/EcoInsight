fbf9d2007e7bd01970502f64aae866b32b2fd5b4
holelung <junho335276@gmail.com>
2025-04-16 12:20:00 +0900

    adminDashBoard editing..

--- frontend/eco-insight/src/components/Layout/AdminLayout.jsx
+++ frontend/eco-insight/src/components/Layout/AdminLayout.jsx
@@ -1,13 +1,20 @@
 import { Outlet, useNavigate } from "react-router-dom";
+import logo from "../../assets/EcoInsigthLogo2.png";
 
 const AdminLayout = () => {
   const navi = useNavigate();
 
+  const clickLogout = () => {
+    localStorage.clear();
+    navi("/");
+  }
+
   return (
     <div className="flex min-h-screen bg-gray-100">
       {/* 사이드바 */}
-      <aside className="w-60 bg-white shadow-md px-4 py-6">
-        <h2 className="text-xl font-bold mb-8">관리자 메뉴</h2>
+      <aside className="w-60 bg-white shadow-md px-4 py-3">
+        <img src={logo} alt="EcoInsightLogo" className="h-14 w-auto" />
+        <h2 className="text-xl font-bold mb-8 mt-5">관리자 메뉴</h2>
         <ul className="space-y-4 text-gray-700">
           <li
             className="cursor-pointer hover:text-green-600"
@@ -34,13 +41,10 @@ const AdminLayout = () => {
       <div className="flex flex-col flex-grow">
         {/* 상단바 */}
         <header className="bg-white shadow px-6 py-4 flex justify-between items-center">
-          <h1 className="text-lg font-semibold">Eco Insight 관리자</h1>
+          <h1 className="text-lg font-semibold">Eco Insight 관리자명</h1>
           <button
-            className="bg-red-500 text-white px-4 py-1 rounded hover:bg-red-600"
-            onClick={() => {
-              localStorage.clear();
-              navi("/");
-            }}
+            className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+            onClick={() => clickLogout()}
           >
             로그아웃
           </button>
