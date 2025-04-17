63c64d30300ab1154aa876828f23dc24dfd3b86d
ggkama <ggkama@naver.com>
2025-04-17 17:20:59 +0900

    아이디/비밀번호 완성, mypage/editprofile 완성, mypost 완성

--- frontend/eco-insight/src/components/Auth/ResetPassword.jsx
+++ frontend/eco-insight/src/components/Auth/ResetPassword.jsx
@@ -5,8 +5,8 @@ import { useNavigate } from 'react-router-dom';
 const ResetPassword = () => {
   const navigate = useNavigate();
 
-  const handleReset = () => {
-    navigate('/changepassword'); // ChangePassword.jsx가 매핑된 경로로 변경하세요
+  const handleMain = () => {
+    navigate('/'); 
   };
 
   return (
@@ -15,14 +15,15 @@ const ResetPassword = () => {
       <main className="flex-grow flex items-center justify-center px-4 py-12">
         <div className="bg-white w-full max-w-md p-8 rounded-2xl shadow-md">
           <p className="text-center text-gray-700 mb-6">
-            로그인을 위하여 비밀번호 재설정이 필요합니다.
+            인증한 이메일로 임시 비밀번호를 보냈습니다.
+            로그인 후 마이페이지에서 비밀번호를 변경해 주세요
           </p>
           <div className="flex justify-center">
             <button
-              onClick={handleReset}
-              className="px-6 py-2 bg-green-400 hover:bg-green-500 text-white rounded-lg transition"
+              onClick={handleMain}
+              className="px-6 py-2 bg-lime-400 hover:bg-green-500 rounded-lg transition"
             >
-              비밀번호 재설정하기
+              홈으로 가기
             </button>
           </div>
         </div>
