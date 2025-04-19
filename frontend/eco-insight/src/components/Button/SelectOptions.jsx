f7904baa46b87e4cc6821117081be7da433aa24a
holelung <junho335276@gmail.com>
2025-04-20 01:29:55 +0900

    Paginagtion 컴포넌트화 및 개선

--- frontend/eco-insight/src/components/Button/SelectOptions.jsx
+++ frontend/eco-insight/src/components/Button/SelectOptions.jsx
@@ -2,6 +2,8 @@
 const SelectOptions = () => {
   return (
     <>
+      <option value={1}>1</option>
+      <option value={5}>5</option>
       <option value={10}>10</option>
       <option value={15}>15</option>
       <option value={20}>20</option>
