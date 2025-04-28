eb9d2885970ae70ff10e5a6c80602d0321ab9ccf
kohyunwoo00 <hyunwoo36003@naver.com>
2025-04-28 17:29:19 +0900

    앞단 매핑값 수정 및 커뮤니티 글쓰기 진행중

--- EcoInsight/src/main/java/com/semi/ecoinsight/community/model/service/CommunityServiceImpl.java
+++ EcoInsight/src/main/java/com/semi/ecoinsight/community/model/service/CommunityServiceImpl.java
@@ -1,5 +1,55 @@
 package com.semi.ecoinsight.community.model.service;
 
-public class CommunityServiceImpl {
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.stereotype.Service;
+
+import com.semi.ecoinsight.admin.model.dto.WriteFormDTO;
+import com.semi.ecoinsight.board.model.dao.BoardMapper;
+import com.semi.ecoinsight.board.model.vo.Attachment;
+import com.semi.ecoinsight.board.model.vo.Board;
+import com.semi.ecoinsight.community.model.dao.CommunityMapper;
+import com.semi.ecoinsight.util.sanitize.SanitizingService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CommunityServiceImpl implements CommunityService{
+	
+	private final SanitizingService sanitizingService;
+	private final CommunityMapper communityMapper;
+	private final BoardMapper boardMapper;
+	
+	public void insertCommunityBoard(WriteFormDTO form) {
+		// XSS 방어(유효성)
+        String sanitizingTitle = sanitizingService.sanitize(form.getTitle());
+        String sanitizingContent = sanitizingService.sanitize(form.getContent());
+        
+        Board board = Board.builder()
+                .memberNo(form.getMemberNo())
+                .categoryId(form.getCategoryId())
+                .boardTitle(sanitizingTitle)
+                .boardContent(sanitizingContent)            
+                .build();
+        
+        communityMapper.insertCommunityBoard(board);
+        
+        Long communityNo = communityMapper.getCommunityNo(form.getMemberNo());
+        if (form.getImageUrls() != null) {
+            List<Attachment> Attachments = form.getImageUrls().stream()
+            .map(url -> Attachment.builder()
+                .boardNo(communityNo)
+                .AttachmentItem(url)
+                .boardType(form.getBoardType())
+                .build()
+                ).collect(Collectors.toList());
+            for (Attachment a : Attachments) {
+                boardMapper.uploadImage(a);
+            }
+        }
+        
+        
+	}
 }
